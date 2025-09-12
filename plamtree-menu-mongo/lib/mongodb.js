89e129f5246a34863a445f4bf52485f7169073cb
maherkhalifa988-wq <maherkhalifa988@gmail.com>
2025-09-12 22:34:50 +0300

    Update mongodb.js

--- plamtree-menu-mongo/lib/mongodb.js
+++ plamtree-menu-mongo/lib/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error("اmongodb+srv://maher:<1234>@anakhel.a3swgn0.mongodb.net/?retryWrites=true&w=majority&appName=anakhel");
+  throw new Error("mongodb+srv://maher:<1234>@anakhel.a3swgn0.mongodb.net/?retryWrites=true&w=majority&appName=anakhel");
 }
 
 let cached = global.mongoose;
